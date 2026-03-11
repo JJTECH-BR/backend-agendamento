1fe7dd4eb02e7e384010024de8138ba40f181468
johnysantos22 <santosjohny302@gmail.com>
2026-03-10 22:35:27 -0300

    feat: finaliza rota de cadastro com validacao de senhas e correcao de escopo

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import bcryptjs from 'bcryptjs';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = express();
 
 // Isso avisa ao nosso servidor que ele vai receber dados no formato JSON (que vamos mandar pelo Insomnia)
@@ -8,6 +11,38 @@ app.use(express.json());
 // Uma rota de teste só para ver se tudo ligou certinho
 app.get('/', (req, res) => {
     return res.json({ message: 'Servidor rodando!' });
+});
+
+app.post('/users', async (req, res) => {
+    const { name, email, password, confirmPassword } = req.body;
+
+    // Verificamos se as senhas coincidem
+    if (password !== confirmPassword) {
+        return res.status(400).json({ error: 'As senhas não coincidem' });
+    };
+
+
+    // Verificamos se esse e-mail já existe no banco (A trava de segurança)
+    const userExists = await prisma.user.findUnique({
+        where: { email: email },
+    });
+
+    if (userExists) {
+        return res.status(400).json({ error: 'Usuário já existe' });
+    };
+
+    const hashedPassword = await bcryptjs.hash(password, 8);
+
+    //Criamos o usuário no banco de dados
+    const user = await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: hashedPassword,
+        }
+    });
+
+    return res.status(201).json(user);
 });
 
 // Ligar o servidor na porta 3333
